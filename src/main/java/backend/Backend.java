39dd974b041ad8ef65dd6d67d0ae56dbcb9d8bb5
Jan <janm.albrecht@googlemail.com>
2020-09-23 01:42:44 +0200

    Structure

---
+++ src/main/java/backend/Backend.java
@@ -0,0 +1,11 @@
+package backend;
+
+public class Backend {
+
+    public void call() {
+        System.out.println("Hier spricht das Backend!");
+    }
+
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.dhbw {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens org.dhbw to javafx.fxml;
+    exports org.dhbw;
+}
