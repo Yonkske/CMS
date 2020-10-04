1d855df76198273f432071514c375b835ce00a38
Jan <janm.albrecht@googlemail.com>
2020-10-04 19:42:48 +0200

    Severe Changes to many files, added DB.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.dhbw {
-
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.dhbw to javafx.fxml;
-    exports org.dhbw;
-}
--- src/main/java/org/dhbw/App.java
+++ src/main/java/org/dhbw/App.java
@@ -12,29 +12,9 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
-
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+public class App  {
 
     public static void main(String[] args) {
-        new Backend().call();
-        launch();
+        new MainController().main(args);
     }
-
 }
