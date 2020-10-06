646fd52afab579625539ce712084ad08b99cada5
Yonkske <49674477+yonkske@users.noreply.github.com>
2020-10-06 15:15:00 +0200

    Infrastructure 2

--- src/test/java/backend/BackendTest.java
+++ src/test/java/backend/BackendTest.java
@@ -1,7 +1,14 @@
 package backend;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 
 public class BackendTest {
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(3, new Backend().add(1,2));
+    }
 
 }
