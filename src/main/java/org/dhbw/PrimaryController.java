dc9e4427ea34947cdfb291a30fc2d035259c567b
Yonkske <49674477+yonkske@users.noreply.github.com>
2020-10-07 22:26:17 +0200

    deleted dep-red-pom and added to gitignore

--- src/main/java/org/dhbw/PrimaryController.java
+++ src/main/java/org/dhbw/PrimaryController.java
@@ -1,6 +1,8 @@
 package org.dhbw;
 
 import java.io.IOException;
+
+import backend.Backend;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -8,5 +10,7 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         MainController.setRoot("secondary");
+
+
     }
 }
