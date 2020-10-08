31a2a03e784b369480333bc2a418a9c45bc625df
Simon Fröhner <simon@wk-froehner.de>
2020-10-08 23:16:55 +0200

    Added methode headers to caller classes

--- src/main/java/backend/database/DbCallerUser.java
+++ src/main/java/backend/database/DbCallerUser.java
@@ -1,4 +1,34 @@
 package backend.database;
 
+import backend.usability.User;
+
+import java.util.ArrayList;
+
 public class DbCallerUser extends DbConnector{
+
+    public User getUser(String name) {
+
+        return null;
+    }
+
+    public boolean insertUser(User user) {
+
+        return false;
+    }
+
+    public boolean updateUser(User user) {
+
+        return false;
+    }
+
+    public boolean deleteUser(User user) {
+
+        return true;
+    }
+
+    public ArrayList<User> getAllUsers() {
+
+        return null;
+    }
+
 }
