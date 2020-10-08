1f78e4d1600bc381c9299b1fc3d244faab136a6d
tabascoel <wi19060@lehre.dhbw-stuttgart.de>
2020-10-08 23:30:32 +0200

    Kommentare und JavaDocs zu Code hinzugefügt.

--- src/main/java/backend/usability/Cit.java
+++ src/main/java/backend/usability/Cit.java
@@ -1,4 +1,4 @@
-// Autor: Ion Tabyrca
+//Autor: Ion Tabyrca
 package backend.usability;
 
 import backend.database.DbCallerCit;
@@ -6,10 +6,14 @@ import backend.database.DbCallerCit;
 public class Cit {
 
     private int id;
-    private String cit_bezeichnung;
+    private String cit_bezeichnung;                             //
     private String[] attributes;
 
-
+    /** Constructor to create an object of CIT
+     *
+     * @param cit_ID is the current id of the CIT
+     * @param attributeList is the list of attributes which are defined by admin
+     */
     public Cit(int cit_ID, String[] attributeList){
         id = cit_ID;                                            //id wird über Konstruktor zugeteilt
         attributes = new String[8];                             // Erzeugen des Attribute-Arrays
@@ -19,25 +23,46 @@ public class Cit {
 
         for (int i = 2; i <= attributeList.length; i++) {       //Alle anderen Attribute werden über den Übergabeparameter weitergegeben.
             this.attributes[i] = attributeList[i-1];
-            System.out.println(attributes[i]);                  //Hilfestellung zur überprüfung der Attribute
+            System.out.println(attributes[i]);                  //Hilfestellung zur Überprüfung der Attribute
         }
 
     }
 
-
+    /**
+     * Shows the CIT of the given ID
+     *
+     * @param id of the CIT to be shown
+     * @return the CIT matching the id
+     */
     public Cit show(int id){
         return DbCallerCit.getCit(id);                          //Methode des DBCallerCit wird aufgerufen um Eintrag zu holen.
 
     }
 
+    /**
+     *Delete the CIT of the given ID
+     *
+     * @param id of the CIT to be deleted
+     * @return boolean either it was successfully or not
+     */
     public boolean delete(int id){
         return DbCallerCit.deleteCit(id);                       //Methode des DBCallerCit wird aufgerufen um Eintrag zu löschen.
     }
 
+    /**
+     *Shows all CIT
+     *
+     * @return a list of CIT which are stored in the database
+     */
     public static Cit[] showAll(){                              //Methode des DBCallerCit wird aufgerufen um alle Einträge zu zeigen.
         return DbCallerCit.getAllCits();
     }
 
+    /**
+     *Get the number of records in the CIT table
+     *
+     * @return a number of records which are stored in the database
+     */
     public static int getCount(){                               //Methode des DBCallerCit wird aufgerufen Anzahl an Einträgen zu zeigen.
         return DbCallerCit.getCount();
     }
