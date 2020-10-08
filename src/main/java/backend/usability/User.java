9f146f086529fb6610912445520e7870c792f2ba
BeekeWiltfang <wi19253@lehre.dhbw-stuttgart.de>
2020-10-08 15:04:40 +0200

    integration of JavaDocs (not complete yet); creation of a constructor; first try at create method

--- src/main/java/backend/usability/User.java
+++ src/main/java/backend/usability/User.java
@@ -9,41 +9,94 @@ public class User {
     private static boolean isAdmin;
     private static boolean isInitial;
 
+    /**
+     * @param userName
+     * @param name
+     * @param surname
+     * @param password
+     * @param isAdmin
+     * @param isInitial
+     */
+    public User(String userName, String name, String surname, String password, boolean isAdmin, boolean isInitial) {
+
+    }
+
+    /**
+     * @param userName
+     * @return User
+     */
     public static Object getUser(String userName) {
         
         return User;
     }
 
+    /**
+     * @param userName
+     * @param name
+     * @param surname
+     * @param password
+     * @param isAdmin
+     * @return User
+     */
     public static Object create(String userName, String name, String surname, String password, boolean isAdmin) {
+
+        User = new User(userName, name, surname, password, isAdmin, true);
 
         return User;
     }
 
+    /**
+     * @param userName
+     * @param isAdmin
+     * @return isAdmin
+     */
     public static boolean changeAdmin(String userName, boolean isAdmin) {
 
         return isAdmin;
     }
 
+    /**
+     * @param userName
+     * @return true
+     */
     public static boolean checkIsAdmin(String userName) {
 
         return true;
     }
 
+    /**
+     * @param userName
+     * @return true
+     */
     public static boolean deleteUser(String userName) {
 
         return true;
     }
 
+    /**
+     * @param userName
+     * @param newPassword
+     * @return true
+     */
     public static boolean changePassword(String userName, String newPassword) {
 
         return true;
     }
 
+    /**
+     * @param userName
+     * @param oldPassword
+     * @param newPassword
+     * @return true
+     */
     public static boolean changePassword(String userName, String oldPassword, String newPassword) {
 
         return true;
     }
 
+    /**
+     * @return true
+     */
     public static boolean logOut() {
 
         return true;
