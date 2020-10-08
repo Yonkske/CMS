6d17dc412b238d128dc71c24c7f49d755abe1760
BeekeWiltfang <wi19253@lehre.dhbw-stuttgart.de>
2020-10-08 21:09:29 +0200

    created the methods of DbCallerUser without any functionality. Included JavaDocs.

--- src/main/java/backend/database/DbCallerUser.java
+++ src/main/java/backend/database/DbCallerUser.java
@@ -1,4 +1,48 @@
 package backend.database;
 
+import org.h2.engine.User;
+
 public class DbCallerUser {
+
+    /**
+     * Method for the query to get the user out if the database.
+     * @param userName - this user should be got
+     * @return userToGet - returns the user from the database
+     */
+    public static User getUser (String userName) {
+
+        User userToGet = null;
+        return userToGet;
+    }
+
+    /**
+     * Method to write a new user into the database.
+     * @param user - this user should be saved
+     * @return boolean - if true: user was saved, if false: user can´t be saved
+     */
+    public static boolean createUser(User user) {
+
+        return true;
+    }
+
+    /**
+     * Method to update a user being saved in the database.
+     * @param user - this user should be updated
+     * @return boolean - if true: user was updated, if false: user can´t be updated
+     */
+    public static boolean updateUser(User user) {
+
+        return true;
+    }
+
+    /**
+     * Method to delete user from the database.
+     * @param user - this user should be deleted
+     * @return boolean - if true: user was deleted, if false: user can´t be deleted
+     */
+    public static boolean deleteUser(User user) {
+
+        return true;
+    }
+
 }
