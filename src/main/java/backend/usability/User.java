9691d28299f33c5fb97fa6da084c2fe921df6cbd
BeekeWiltfang <wi19253@lehre.dhbw-stuttgart.de>
2020-10-08 16:29:46 +0200

    worked on methods changePW

--- src/main/java/backend/usability/User.java
+++ src/main/java/backend/usability/User.java
@@ -18,7 +18,7 @@ public class User {
      * @param isAdmin - specification if the future user is an admin
      * @param isInitial - specification if the password is that the admin gave the future user or if he already changed his password
      */
-    public User(String userName, String name, String surname, String password, boolean isAdmin, boolean isInitial) {
+    private User(String userName, String name, String surname, String password, boolean isAdmin, boolean isInitial) {
         this.userName = userName;
         this.name = name;
         this.surName = surname;
@@ -89,11 +89,21 @@ public class User {
      * Within this method the admin can give a new password to the user if he forgot his.
      * @param userName - username of the user whose password should be changed
      * @param newPassword - new password given by the admin
-     * @return isInitial - if the password was changed by the admin it applies to be an new initial password
+     * @return boolean - if the password was changed: true, else: false
      */
-    public static boolean changePassword(String userName, String newPassword) {
+    public boolean changePassword(String userName, String newPassword) {
 
-        return isInitial;
+        User userToChangePwByAdmin= getUser(userName);
+
+        if (userToChangePwByAdmin.userName == userName) {
+            userToChangePwByAdmin.password = newPassword;
+            userToChangePwByAdmin.isInitial = true;
+
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     /**
@@ -101,11 +111,22 @@ public class User {
      * @param userName - username of the user whose password should be changed
      * @param oldPassword - the old password that should be changed
      * @param newPassword - the new password
-     * @return true - if true returns the password was changed, if false returns the password could not be changed
+     * @return boolean - if true returns the password was changed, if false returns the password could not be changed
      */
-    public static boolean changePassword(String userName, String oldPassword, String newPassword) {
+    public boolean changePassword(String userName, String oldPassword, String newPassword) {
+    // TODO: functionality when oldPassword != userToChangePw.password
 
-        return true;
+        User userToChangePw= getUser(userName);
+
+        if (userToChangePw.userName == userName & userToChangePw.password == oldPassword) {
+            userToChangePw.password = newPassword;
+
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 
     /**
