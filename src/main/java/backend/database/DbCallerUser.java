3a106fc598f6ad004b99ec2f119bad4937d81fca
Simon Fröhner <simon@wk-froehner.de>
2020-10-09 16:09:22 +0200

    Added javadoc to DbCallerUser

--- src/main/java/backend/database/DbCallerUser.java
+++ src/main/java/backend/database/DbCallerUser.java
@@ -6,22 +6,42 @@ import java.util.ArrayList;
 
 public class DbCallerUser extends DbConnector{
 
-    public User getUser(String name) {
+    /**
+     * Method for the query to get the user out if the database.
+     * @param userName - this user should be got
+     * @return userToGet - returns the user from the database
+     */
+    public User getUser(String userName) {
 
         return null;
     }
 
-    public boolean insertUser(User user) {
+    /**
+     * Method to write a new user into the database.
+     * @param userToCreate - this user should be saved
+     * @return boolean - if true: user was saved, if false: user can´t be saved
+     */
+    public boolean insertUser(User userToCreate) {
 
         return false;
     }
 
-    public boolean updateUser(User user) {
+    /**
+     * Method to update a user being saved in the database.
+     * @param userToUpdate - this user should be updated
+     * @return boolean - if true: user was updated, if false: user can´t be updated
+     */
+    public boolean updateUser(User userToUpdate) {
 
         return false;
     }
 
-    public boolean deleteUser(User user) {
+    /**
+     * Method to delete user from the database.
+     * @param userToDelete - this user should be deleted
+     * @return boolean - if true: user was deleted, if false: user can´t be deleted
+     */
+    public boolean deleteUser(User userToDelete) {
 
         return false;
     }
