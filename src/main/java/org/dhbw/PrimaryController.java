c565ee57637a0e1af9a1c85cbb9bf33bbdf648f8
Celine Stock <stock_celine@web.de>
2020-10-09 21:03:29 +0200

    Basic Layout zu Seiten hinzugefügt

--- src/main/java/org/dhbw/PrimaryController.java
+++ src/main/java/org/dhbw/PrimaryController.java
@@ -9,7 +9,7 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        MainController.setRoot("secondary");
+        MainController.setRoot("startpageUser");
 
 
     }
