3c66d2e548afc0cd4067a18d8708de8248004dfa
evelyn <Evelyn-stoll@gmx.de>
2020-10-12 21:07:45 +0200

    weiterleitung zur Startseite, CIT Oberfläche, Statistik und zur CIT erstellen.

--- src/main/java/org/dhbw/CITAdminController.java
+++ src/main/java/org/dhbw/CITAdminController.java
@@ -1,4 +1,25 @@
 package org.dhbw;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOError;
+import java.io.IOException;
+
 public class CITAdminController extends Controller {
+
+    public void swapToStartpageAdmin(ActionEvent actionEvent) throws IOException {
+        FXMLFactory.setRoot("StartpageAdmin.fxml");
+    }
+
+    public void swapToCITAdmin(ActionEvent actionEvent) throws IOException{
+        FXMLFactory.setRoot("CITAdmin.fxml");
+    }
+
+    public void swapToStatisticAdmin(ActionEvent actionEvent) throws IOException {
+        FXMLFactory.setRoot("StatisticAdmin");
+    }
+
+    public void swapToCITAdd(ActionEvent actionEvent) throws IOException {
+        FXMLFactory.setRoot("CITAdd");
+    }
 }
