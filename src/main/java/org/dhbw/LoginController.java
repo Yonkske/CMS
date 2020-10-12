4dddda7de4fdec329dadbeff3f10e9b70bf51d7f
BeekeWiltfang <wi19253@lehre.dhbw-stuttgart.de>
2020-10-12 20:45:59 +0200

    mainly created methods

--- src/main/java/org/dhbw/LoginController.java
+++ src/main/java/org/dhbw/LoginController.java
@@ -1,5 +1,32 @@
 package org.dhbw;
 
+import backend.usability.User;
+
 public class LoginController extends Controller {
+
+    public void logIn(String userName, String password) {
+
+    }
+
+    private void showError(String errorMessage) {
+
+    }
+
+    private void switchToStartpage(User user) {
+
+    }
+
+    private void openPopUpEditPassword(User user) {
+
+    }
+
+    public void exit() {
+
+    }
+
+    private User getUser(String userName) {
+
+        return null;
+    }
 
 }
