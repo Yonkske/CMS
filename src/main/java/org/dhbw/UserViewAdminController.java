37700b5cd3d7f74d6e20a2fc6dbb993b4104e849
Nicole Widmayer <nicewidmayer@yahoo.de>
2020-10-14 15:15:16 +0200

    PopUp's öffnen sich aus dem PopUp UserViewAdmin.fxml

--- src/main/java/org/dhbw/UserViewAdminController.java
+++ src/main/java/org/dhbw/UserViewAdminController.java
@@ -3,23 +3,19 @@ package org.dhbw;
 
 import backend.usability.User;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.fxml.Initializable;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 
-public class UserViewAdminController extends Controller implements Initializable  {
+
+public class UserViewAdminController extends Controller  {
 
     @FXML
     private TextField surnameTf;
@@ -71,8 +67,15 @@ public class UserViewAdminController extends Controller implements Initializable
         NotificationController NotificationController = new NotificationController();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Notification.fxml"));
         loader.setController(NotificationController);
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        scene.getWindow().sizeToScene();
+        stage.show();
 
-        yesBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+        /*yesBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 CB_CALLER_USER.deleteUser(CB_CALLER_USER.getUser(userName));
@@ -85,9 +88,10 @@ public class UserViewAdminController extends Controller implements Initializable
             public void handle(ActionEvent actionEvent) {
                 new Controller().closeScene();
             }
-        });
+        });*/
     }
 
+    @FXML
     /**
      *
      * @param actionEvent
@@ -95,26 +99,11 @@ public class UserViewAdminController extends Controller implements Initializable
      */
     public void switchToUserEditAdmin(ActionEvent actionEvent) throws IOException {
         // TODO: Test der Methode
-        User user = User.getUser(usernameTf.getText());
+        String userName = usernameTf.getText();
 
-        UserEditAdminController UserEditAdminController = new UserEditAdminController();
+        //UserEditAdminController UserEditAdminController = new UserEditAdminController();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("UserEditAdmin.fxml"));
-        loader.setController(UserEditAdminController);
-
- //       UserEditAdminController.showUser(user);
-    }
-    @FXML
-    /**
-     *
-     * @throws IOException
-     */
-    public void switchToPasswordEditAdmin() throws IOException {
-        // TODO: Methode testen
-        //User user = User.getUser(usernameTf.getText());
-
-        PasswordEditAdminController fenster = new PasswordEditAdminController();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("PasswordEditAdmin.fxml"));
-        loader.setController(fenster);
+        //loader.setController(UserEditAdminController);
         Parent root = loader.load();
         Stage stage = new Stage();
         Scene scene = new Scene(root);
@@ -122,12 +111,31 @@ public class UserViewAdminController extends Controller implements Initializable
         scene.getWindow().sizeToScene();
         stage.show();
 
- //       PasswordEditAdminController.showUserName(user);
-
+        // user = User.getUser(usernameTf.getText());
+        //UserEditAdminController.showUser(user);
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    /**
+     *
+     * @param actionEvent
+     * @throws IOException
+     */
+    public void switchToPasswordEditAdmin(ActionEvent actionEvent) throws IOException {
+        // TODO: Methode testen
+        User user = User.getUser(usernameTf.getText());
+
+        //PasswordEditAdminController PasswordEditAdminController = new PasswordEditAdminController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PasswordEditAdmin.fxml"));
+        //loader.setController(PasswordEditAdminController);
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        scene.getWindow().sizeToScene();
+        stage.show();
+
+        //PasswordEditAdminController.showUserName(user);
 
     }
 }
