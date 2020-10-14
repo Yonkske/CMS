8fef7e91e97edc49a77eafb85a9a1d49483482c9
tabascoel <wi19060@lehre.dhbw-stuttgart.de>
2020-10-14 20:49:17 +0200

    Weiterleitung von Statistik zu anderen Views implementiert

--- src/main/java/org/dhbw/StatisticController.java
+++ src/main/java/org/dhbw/StatisticController.java
@@ -1,4 +1,24 @@
 package org.dhbw;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class StatisticController extends Controller {
+
+    public void swapToStartpage(ActionEvent actionEvent) throws IOException {
+        FXMLFactory.setRoot("Startpage");
+    }
+
+    public void swapToCIT(ActionEvent actionEvent) throws IOException{
+        FXMLFactory.setRoot("CIT");
+    }
+
+    public void swapToStatistic(ActionEvent actionEvent) throws IOException {
+        FXMLFactory.setRoot("Statistic");
+    }
+
+    public void swapToBenutzer(ActionEvent actionEvent) throws IOException {
+        FXMLFactory.setRoot("UserAdmin");
+    }
 }
