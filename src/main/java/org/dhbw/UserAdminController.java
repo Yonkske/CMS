7997a37282f964473f163cf85e0469f33e9e5aab
evelyn <Evelyn-stoll@gmx.de>
2020-10-15 13:46:28 +0200

    Erstellung der Statusleiste in Statistik

--- src/main/java/org/dhbw/UserAdminController.java
+++ src/main/java/org/dhbw/UserAdminController.java
@@ -1,8 +1,19 @@
 package org.dhbw;
 
+import backend.usability.Cir;
+import backend.usability.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class UserAdminController extends Controller {
 
@@ -22,4 +33,5 @@ public class UserAdminController extends Controller {
     public void swapToBenutzer(ActionEvent actionEvent) throws IOException {
         FXMLFactory.setRoot("UserAdmin");
     }
+
 }
