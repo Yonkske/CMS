f64cc7c67763163b9fce7b8bef167bde34d29b99
Jan <janm.albrecht@googlemail.com>
2020-10-17 18:08:59 +0200

    Added Check for Null, so no empty CITs are possible

--- src/main/java/org/dhbw/CITAddController.java
+++ src/main/java/org/dhbw/CITAddController.java
@@ -1,10 +1,7 @@
 package org.dhbw;
 
-import backend.database.DbCallerCir;
-import backend.database.DbCallerCit;
 import backend.usability.Cit;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -14,6 +11,7 @@ import javafx.stage.Stage;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 
@@ -78,31 +76,30 @@ public class CITAddController extends Controller implements Initializable {
 
     @FXML
     public void fillIn(ActionEvent actionEvent) throws SQLException {
+        if (Objects.isNull(citTf.getText())) {
+            String[] sCitArray = new String[8];
+            int id = Integer.parseInt(idTf.getText());
 
-        String[] sCitArray = new String[8];
-        int id = Integer.parseInt(idTf.getText());
+            sCitArray[0] = citTf.getText();
+            sCitArray[1] = attribut1Tf.getText();
+            sCitArray[2] = attribut2Tf.getText();
+            sCitArray[3] = attribut3Tf.getText();
+            sCitArray[4] = attribut4Tf.getText();
+            sCitArray[5] = attribut5Tf.getText();
+            sCitArray[6] = attribut6Tf.getText();
+            sCitArray[7] = attribut7Tf.getText();
+            if (sCitArray[0].length() == 0) {
+                showError();
+            } else {
 
-        sCitArray[0] = citTf.getText();
-        sCitArray[1] = attribut1Tf.getText();
-        sCitArray[2] = attribut2Tf.getText();
-        sCitArray[3] = attribut3Tf.getText();
-        sCitArray[4] = attribut4Tf.getText();
-        sCitArray[5] = attribut5Tf.getText();
-        sCitArray[6] = attribut6Tf.getText();
-        sCitArray[7] = attribut7Tf.getText();
-        if (sCitArray[0].length()== 0){
-            showError();
+                Cit cit = new Cit(id, sCitArray);
+                DB_CALLER_CIT.createCit(cit);
+                Stage stClose = new Stage();
+                stClose = (Stage) submitBtn.getScene().getWindow();
+                stClose.close();
+            }
+
         }
-        else {
-
-            Cit cit = new Cit(id, sCitArray);
-            DB_CALLER_CIT.createCit(cit);
-            Stage stClose = new Stage();
-            stClose = (Stage) submitBtn.getScene().getWindow();
-            stClose.close();
-        }
-
-
     }
 
     @FXML
