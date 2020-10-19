d2835c80606da75faeee68234a3b5e56628540b6
Jan <janm.albrecht@googlemail.com>
2020-10-19 15:09:38 +0200

    Dies das Ananas

--- src/main/java/org/dhbw/UserAdminController.java
+++ src/main/java/org/dhbw/UserAdminController.java
@@ -1,8 +1,6 @@
 package org.dhbw;
 
-import backend.database.DbCallerUser;
 import backend.usability.User;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -270,14 +268,50 @@ public class UserAdminController extends MainPagesController {
     public void setTableWithFilterAndSearch() {
         String selectedUser = filterUser.getSelectionModel().getSelectedItem();
         String searchValue = searchTextField.getText();
+        ArrayList<User> filteredList = new ArrayList<>();
 
         if (selectedUser.equals("Rechte") && searchValue.length() != 0) {
-            this.setTableContent(this.getAllUserSearchValue(searchValue));
+           // this.setTableContent(this.getAllUserSearchValue(searchValue));
+          //  allUsers.stream().filter(user -> user.getUserName().contains(searchValue)).findAny().ifPresent(filteredList::add); // finds first
+
+            ArrayList<User> containQuery = new ArrayList<>();
+            allUsers.forEach(user1 -> {
+                if (user1.getUserName().contains(searchValue)) {
+                    containQuery.add(user1);
+                }
+            });
+            containQuery.forEach(user2 -> System.out.println(user2.getUserName())); // finds all
+            this.setTableContent(containQuery);
+
         } else if (!selectedUser.equals("Rechte")) {
-            this.setTableContent(this.getAllWithFilterAndSearch(selectedUser, searchValue));
+           // this.setTableContent(this.getAllWithFilterAndSearch(selectedUser, searchValue));
+           // allUsers.stream().filter(user -> user.getRight().equals(selectedUser)).filter(user -> user.getUserName().contains(searchValue)).findAny().ifPresent(filteredList::add);
+            ArrayList<User> memory = new ArrayList<>();
+            ArrayList<User> containQuery = new ArrayList<>();
+            allUsers.forEach(user1 -> {
+                if (user1.getUserName().contains(searchValue)) {
+                    memory.add(user1);
+                }
+            });
+            memory.forEach(user3 -> {
+                if (user3.getRight().equals(selectedUser)) {
+                    containQuery.add(user3);
+                }
+            });
+            memory.forEach(user2 -> System.out.println(user2.getUserName() + " Memory"));
+            containQuery.forEach(user2 -> System.out.println(user2.getUserName() + " final"));
+
+            this.setTableContent(containQuery);
+
+
+
         } else {
             this.setTableContent(allUsers);
         }
+
+
+        filteredList.stream().forEach(user -> System.out.println(user.getUserName()));
+
     }
 
     @Override
