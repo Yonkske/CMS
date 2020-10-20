9dfe894af00cac9b6416e2b7141d1a8fc6e75fd0
Melissa Negele <melissanegele@yahoo.de>
2020-10-20 16:51:13 +0200

    CIRAdd.fxml - Felder angzeigen lassen, wenn es dieses Attribut gibt

--- src/main/java/org/dhbw/MainCirPopupsController.java
+++ src/main/java/org/dhbw/MainCirPopupsController.java
@@ -76,12 +76,33 @@ public abstract class MainCirPopupsController extends Controller implements Init
         // Labels für CIT setzen
         Cit cit = cir.getCit();
         attribut1Lbl.setText(cit.getCitAttributes()[1]);
+        if(attribut1Lbl.getText().length()>0){
+            attribut1Tf.setVisible(true);
+        }
         attribut2Lbl.setText(cit.getCitAttributes()[2]);
+        if(attribut2Lbl.getText().length()>0){
+            attribut2Tf.setVisible(true);
+        }
         attribut3Lbl.setText(cit.getCitAttributes()[3]);
+        if(attribut3Lbl.getText().length()>0){
+            attribut3Tf.setVisible(true);
+        }
         attribut4Lbl.setText(cit.getCitAttributes()[4]);
+        if(attribut4Lbl.getText().length()>0){
+            attribut4Tf.setVisible(true);
+        }
         attribut5Lbl.setText(cit.getCitAttributes()[5]);
+        if(attribut5Lbl.getText().length()>0){
+            attribut5Tf.setVisible(true);
+        }
         attribut6Lbl.setText(cit.getCitAttributes()[6]);
+        if(attribut6Lbl.getText().length()>0){
+            attribut6Tf.setVisible(true);
+        }
         attribut7Lbl.setText(cit.getCitAttributes()[7]);
+        if(attribut7Lbl.getText().length()>0){
+            attribut7Tf.setVisible(true);
+        }
 
     }
 }
